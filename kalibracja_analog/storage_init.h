e56a93261f8907ed94a2998ead6053b64c9c747d
v33r005 <125038833+v33r005@users.noreply.github.com>
2025-08-11 18:45:56 +0200

    Update storage_init.h

--- kalibracja_analog/storage_init.h
+++ kalibracja_analog/storage_init.h
@@ -15,8 +15,8 @@
   if (cfg->getInt32(PARAM2, &min_zawor)) {} else {}
   if (cfg->getInt32(PARAM3, &min_pompa)) {} else {}
   if (cfg->getInt32(PARAM4, &Min_Analog_Odczyt)) {} else {}
-  if (cfg->getInt32(PARAM5, &Gorna_Wartosc)) {} else {}
-  if (cfg->getInt32(PARAM6, &Dolna_Wartosc)) {} else {}
+  if (cfg->getUInt32(PARAM5, &Gorna_Wartosc)) {} else {}
+  if (cfg->getUInt32(PARAM6, &Dolna_Wartosc)) {} else {}
   if (cfg->getInt32(PARAM7, &Max_Analog_Odczyt)) {} else {}
   if (cfg->getInt32(PARAM8, &Sygnal_Alarmu)) {} else {}
   if (cfg->getInt32(PARAM9, &Min_Wartosc)) {} else {}
@@ -26,3 +26,4 @@
   if (cfg->getUInt8(STANDBY_MODE1, &standby_mode1)) {} else {}
 
 #endif
+
