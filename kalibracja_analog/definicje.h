9761a6ae0d923635ba6e82f04da42719aa853ae2
v33r005 <125038833+v33r005@users.noreply.github.com>
2025-08-14 18:57:28 +0200

    Update definicje.h

--- kalibracja_analog/definicje.h
+++ kalibracja_analog/definicje.h
@@ -10,12 +10,13 @@ constexpr uint8_t PUMP_GPIO = 12;
 constexpr uint8_t VALVE_GPIO = 13;
 constexpr uint8_t PUMP_BTN_GPIO = 14;
 constexpr uint8_t VALVE_BTN_GPIO = 15;
-constexpr uint8_t SENSOR_MAX_GPIO = 16;
-constexpr uint8_t SENSOR_MIN_GPIO = 17;
-constexpr uint8_t LED_PUMP_GPIO = 18;
-constexpr uint8_t LED_VALVE_GPIO = 19;
+constexpr uint8_t SENSOR_MAX_GPIO = 3;
+constexpr uint8_t SENSOR_MIN_GPIO = 4;
+constexpr uint8_t LED_PUMP_GPIO = 5;
+constexpr uint8_t LED_VALVE_GPIO = 6;
 
 constexpr uint16_t DEFAULT_IN_MAX  = 4'095;
 constexpr uint16_t DEFAULT_IN_MIN = 0;
 constexpr int32_t DEFAULT_NAN_VALUE = -1;
+
 
