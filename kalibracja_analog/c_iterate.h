d58612ce05d8fa7e908b2d836645b601236c5083
v33r005 <125038833+v33r005@users.noreply.github.com>
2025-08-14 07:31:12 +0200

    Change read analog value method

--- kalibracja_analog/c_iterate.h
+++ kalibracja_analog/c_iterate.h
@@ -5,29 +5,13 @@ class CustomIterate : public Supla::Element {
   CustomIterate() {}
 
   void iterateAlways() {
-
-    if (millis() - lastReadTime_ > 2000) {
-      lastReadTime_ = millis();
-      aktualnyPoziom = map(analogRead(PIN_ANALOGOWY), 
-                                           dolnaWartosc, gornaWartosc, 0 , 100);
-      /*
-      if (Aktualny_Poziom > 100) {
-        zbiornik->setValue(100);
-      } else if (Aktualny_Poziom < 0) {
-        zbiornik->setValue(0);
-      } else {
-        zbiornik->setValue(Aktualny_Poziom);
-      }
-      */
-      zbiornik->setValue(aktualnyPoziom);
-    }
     Kalibracja();
-    if (pominZabezpieczenia->isOn() && aktualnyPoziom < sygnalAlarmu) {
+    if (pominZabezpieczenia->isOn() && zbiornik->getValue() < sygnalAlarmu) {
       if (uruchomZawor->isOn()) {
         uruchomZawor->turnOff();
       }
       zawor->turnOn();
-    } else if (pominZabezpieczenia->isOn() && aktualnyPoziom >= sygnalAlarmu) {
+    } else if (pominZabezpieczenia->isOn() && zbiornik->getValue >= sygnalAlarmu) {
       zawor->turnOff();
       pominZabezpieczenia->turnOff();
     }
@@ -36,16 +20,15 @@ class CustomIterate : public Supla::Element {
         zawor->turnOff();
       }
     }
-    if (uruchomZawor->isOn() && aktualnyPoziom <= minZawor) {
+    if (uruchomZawor->isOn() && zbiornik->getValue() <= minZawor) {
       zawor->turnOn();
-    } else if(uruchomZawor->isOn() && aktualnyPoziom >= maxZawor) {
+    } else if(uruchomZawor->isOn() && zbiornik->getValue() >= maxZawor) {
       zawor->turnOff();
     }
     ObslugaPompy();
   }
 
  protected:
-  uint32_t lastReadTime_ = 0;
 
 };  //  CustomIterate
 
