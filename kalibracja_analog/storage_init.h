7ffed91f032bc08a02807fbfd7f3b21e71f9fc04
v33r005 <125038833+v33r005@users.noreply.github.com>
2025-08-16 09:09:17 +0200

    Update storage_init.h

--- kalibracja_analog/storage_init.h
+++ kalibracja_analog/storage_init.h
@@ -11,13 +11,9 @@ if (Supla::Storage::ConfigInstance()->getString(DEV_NAME, devName, 30)) {
 cfg->getUInt32(PARAM1, &maxZawor);
 cfg->getUInt32(PARAM2, &minZawor);
 cfg->getUInt32(PARAM3, &minPompa);
-cfg->getInt32(PARAM4, &minAnalogOdczyt);
 cfg->getUInt32(PARAM5, &gornaWartosc);
 cfg->getUInt32(PARAM6, &dolnaWartosc);
-cfg->getInt32(PARAM7, &maxAnalogOdczyt);
 cfg->getUInt32(PARAM8, &sygnalAlarmu);
-cfg->getUInt8(PARAM11, &zalaczAnalog);
 cfg->getUInt8(STANDBY_MODE, &standbyMode);
 cfg->getUInt8(STANDBY_MODE1, &standbyMode1);
 cfg->getUInt32(NAN_VALUE, &nanValue);
-
